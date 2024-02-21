9faf9ce708204449aeeb67401a3faf149664f5a7
yosra <yosramaihi1@gmail.com>
2024-02-21 15:55:59 +0100

    button clicker

---
+++ Web Fundamentals Part Time/JS/Core/Button Clicker/script.js
@@ -0,0 +1,17 @@
+
+ console.log("page loading");
+var btn3 = document.querySelector('.btn3')
+var btn=document.querySelector('.btn')
+function change(){
+    btn.innerText="log out";
+
+}
+    
+
+
+
+
+
+function add(){
+    btn3.remove();
+}

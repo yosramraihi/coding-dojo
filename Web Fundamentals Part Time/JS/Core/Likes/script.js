89ad1b2e917f82be38dc5870155e959f7072cf87
yosra <yosramaihi1@gmail.com>
2024-02-22 00:13:55 +0100

    likes

---
+++ Web Fundamentals Part Time/JS/Core/Likes/script.js
@@ -0,0 +1,12 @@
+console.log("page loading")
+
+var count=3;
+var output=document.getElementById('output');
+
+function countclick(){
+    count =count + 1;
+output.innerHTML=count;
+
+}
+
+
---
+++ Web Fundamentals Part Time/JS/Core/Likes/script2.js
@@ -0,0 +1,25 @@
+console.log("page loading")
+
+var count1=9;
+var output=document.getElementById('output');
+
+function countclick1(){
+    count1 =count1 + 1;
+output.innerHTML=count1;
+
+}
+var count2=12;
+var output2=document.getElementById('output2');
+
+function countclick2(){
+    count2 =count2 + 1;
+output2.innerHTML=count2;
+
+}
+var count3=9;
+var output3=document.getElementById('output3');
+
+function countclick3(){
+    count3 =count3 + 1;
+output3.innerHTML=count3;
+}
